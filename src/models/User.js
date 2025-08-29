d4d29c9ed36bf855e346be546efa1194ce7cec5e
rg23692369 <rg23692369@gmail.com>
2025-08-29 15:36:10 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true }, // display name
-  Phone: { type: Number, required: true, unique: true },
+  Mobile: { type: Number, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ["user", "astrologer", "admin"], default: "user" }
 }, { timestamps: true });
